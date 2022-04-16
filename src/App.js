e1cd1eccfd27d7f89bcef7f8129348d73f2eec1f
PiedraQV <piedraqv@gmail.com>
2022-04-16 18:42:01 -0500

    Empezando(PrimerosArchivos)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='images\store_black_24dp.svg' className="Icon-home"></img>
+        <h1>Fractal Store</h1>
+        <p>Abrimos pronto...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
