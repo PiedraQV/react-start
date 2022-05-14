a6ccd494ce801d22f03a8d0e383574d3536c4cd0
PiedraQV <piedraqv@gmail.com>
2022-05-14 16:58:36 -0500

    Sincronizar counter

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 import { useCartContext } from "./CartContext";
 import ItemCount from "./ItemCount";
+import { useState } from "react";
+
 
 const ItemDetail = ({detalle}) =>{
 
   const { addToCart, deleteCart } = useCartContext();
 
+  const [terminar, setTerminar] = useState (false)
+
   function handleOnAdd(count) {
+    setTerminar(true)
     console.log(count);
     addToCart(detalle, count);
   }
@@ -23,7 +29,13 @@ const ItemDetail = ({detalle}) =>{
       <h2 className='text-2xl py-1'>${detalle.precio}</h2>
       <p>{detalle.description}</p>
       {
-          <ItemCount initial={1} exist={detalle.exist} onAdd={handleOnAdd} deleteE={borrarCarro}/>
+        terminar ?
+        (
+          <Link to="/Pages/CartPage" className="m-20 bg-indigo-900 hover:bg-indigo-300 text-stone-50 py-2 px-5 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          Terminar Compra
+          </Link>
+        ) : (<ItemCount initial={1} exist={detalle.exist} onAdd={handleOnAdd} deleteE={borrarCarro}/>)
+          
       }
       </div>     
     )
